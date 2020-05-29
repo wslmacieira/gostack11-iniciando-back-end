22ea76abf0f537dc6ab27897c61ad5324dc344f6
wslmacieira <wslmacieira@outlook.com>
2020-05-29 13:50:46 -0300

    Upload de arquivos para o Amazon S3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -12,7 +12,7 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticade';
 const usersRouter = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
